406f1581ae4badcd8b6d19a4ffb53be5c6c9ac46
Max Dmitriev <maxdmitriev.dev@gmail.com>
2016-06-24 17:49:22 +0300

    Learn EF 24.06.2016

--- Integration/FirstEF6App/FirstEF6App/Properties/AssemblyInfo.cs
+++ Integration/FirstEF6App/FirstEF6App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AttachmentsModule")]
+[assembly: AssemblyTitle("FirstEF6App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AttachmentsModule")]
+[assembly: AssemblyProduct("FirstEF6App")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4875b584-f8cb-42aa-92ce-8d55873e7696")]
+[assembly: Guid("124660d8-3136-4501-b51b-6929f8c83e4c")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Integration/FirstEF6App/FirstEF6App/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FirstEF6App
+{
+    class UserContext:DbContext
+    {
+        public UserContext()
+            : base("UserDB")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
