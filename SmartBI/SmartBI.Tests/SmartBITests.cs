ac3e8a01ec5ed2eb8c70c0e76122edba82db47d4
Max Dmitriev <maxdmitriev.dev@gmail.com>
2016-11-21 12:44:02 +0300

    21.11.2016

---
+++ SmartBI/SmartBI.Tests/SmartBITests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace SmartBI.Tests
+{
+    [TestClass]
+    public class SmartBITests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML","TestData.xml","User", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(Convert.ToString(TestContext.DataRow["Id"]));
+        }
+        
+    }
+}
